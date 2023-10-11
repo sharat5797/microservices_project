ecf72ae6d9f63570f4abfd503ada0ab659fde03e
Baganna Kumbar <bagannak@testvagrant.com>
2023-10-11 12:35:07 +0530

    added productioIssue class

---
+++ services/production_issue.py
@@ -0,0 +1,12 @@
+from services.measures import Measures
+
+
+class ProductionIssue(Measures):
+    def __init__(self):
+        pass
+
+    def getMeasureResponse(self):
+        pass
+
+    def getSource(self):
+        pass
